780e5d2a5097195cc0ecd4185e9eabc159950018
zhanglei <lei.zhang@sunscell.com>
2017-05-25 18:15:15 +0800

    merge

--- src/app/consumable/consumables.component.ts
+++ src/app/consumable/consumables.component.ts
@@ -7,7 +7,7 @@ import { Consumable } from './consumable';
 import { ConsumableService } from './consumable.service';
 import * as QRCode from 'qrcode';
 import * as _ from 'underscore';
-import 'jquery';
+import * as $ from 'jquery';
 import 'bootstrap';
 
 @Component({
