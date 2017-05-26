03a058bcfaf1a3cd391f29422835b217d2debd0b
zhanglei <lei.zhang@sunscell.com>
2017-05-26 08:45:36 +0800

    clear tesst

--- src/app/testing/router.outlet.stub.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Directive, Injectable, Input, HostListener } from '@angular/core';
-
-// tslint:disable-next-line:max-classes-per-file
-@Component({
-  selector: 'router-outlet',
-  template: ''
-})
-export class RouterOutletStubComponent {
-}
